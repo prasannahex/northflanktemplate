e7fdb7ece3fa708c414da2b6f28ab564245723c7
prasannahex <prasannas4@hexaware.com>
2026-06-02 14:16:18 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from {os.getenv('APP_ENV', 'UNKNOWN')}"
+
+    return render_template(
+        "index.html",
+        environment=os.getenv("APP_ENV", "UNKNOWN")
+    )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
