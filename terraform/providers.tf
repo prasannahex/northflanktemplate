082a193dedc8918da13e4726d05dcb0a8c1ad8b5
prasannahex <prasannas4@hexaware.com>
2026-06-02 10:47:43 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_version = ">= 1.6.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.40"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
