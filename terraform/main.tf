c4ba4f229089bd524d56d705b8c53a177c39c98b
prasannahex <prasannas4@hexaware.com>
2026-06-02 10:58:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,48 @@
+locals {
 
+  env = lower(var.environment)
+
+  resource_group_name = "rg-northflank-${local.env}"
+
+  app_service_plan_name = "asp-northflank-${local.env}"
+
+  webapp_name = "northflank-${local.env}"
+}
+
+resource "azurerm_resource_group" "rg" {
+
+  name     = local.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+
+  name                = local.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "F1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+
+  name                = local.webapp_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  service_plan_id = azurerm_service_plan.plan.id
+
+  https_only = true
+
+  site_config {
+
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+
+  app_settings = {
+    APP_ENV = upper(local.env)
+  }
+}
