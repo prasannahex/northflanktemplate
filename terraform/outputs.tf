afafdaf9fbfd4d34a82ba892e12ad0812a304ab8
prasannahex <prasannas4@hexaware.com>
2026-06-02 10:58:42 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
 
+output "app_service_plan_name" {
+  value = azurerm_service_plan.plan.name
+}
+
+output "webapp_name" {
+  value = azurerm_linux_web_app.webapp.name
+}
+
+output "webapp_url" {
+  value = "https://${azurerm_linux_web_app.webapp.default_hostname}"
+}
