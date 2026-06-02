3e962020981e7461f89e64babf9e89ffd928ffe7
prasannahex <prasannas4@hexaware.com>
2026-06-02 10:57:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,18 @@
+variable "subscription_id" {
+  type = string
+}
 
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+
+  validation {
+    condition     = contains(["dev", "uat", "prod"], lower(var.environment))
+    error_message = "Environment must be dev, uat or prod."
+  }
+}
